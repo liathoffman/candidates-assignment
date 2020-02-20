a9d6186b2f08ab96ac105e7dc609e5f32c5e3cfd
liathoffman <liatshoffman@gmail.com>
2020-02-20 17:52:33 +0200

    decoded the bootstrap script, removed the line causing all traffic to be denied - now the webserver works on bootup

--- vagrant/.bootstrap/bootstrap.sh
+++ vagrant/.bootstrap/bootstrap.sh
@@ -1,8 +1,13 @@
-echo 'IyEvdXNyL2Jpbi9lbnYgYmFzaAoKYXB0LWdldCB1cGRhdGUKYXB0LWdldCBpbnN0YWxsIC15IGFw
-YWNoZTIKCmVjaG8gIjxodG1sPgo8aGVhZGVyPjx0aXRsZT5UaGlzIGlzIHRpdGxlPC90aXRsZT48
-L2hlYWRlcj4KPGJvZHk+CkhlbGxvIHdvcmxkCjwvYm9keT4KPC9odG1sPgoiID4gL3Zhci93d3cv
-aW5kZXguaHRtbApzZWQgIC1pICAncy9BbGxvdyBmcm9tIGFsbC9kZW55IGZyb20gYWxsL2knICAv
-ZXRjL2FwYWNoZTIvc2l0ZXMtYXZhaWxhYmxlL2RlZmF1bHQKc2VydmljZSAgYXBhY2hlMiByZWxv
-YWQgCgoKCmVjaG8gJzEyNy4wLjAuMSB3d3cuYXNjaWktYXJ0LmRlJyA+PiAvZXRjL2hvc3RzCmlw
-IHJvdXRlIGFkZCAyMDguODYuMjI0LjkwLzMyICAgZGV2IGV0aDAgc3JjIDE5Mi4xNjguMTAwLjEw
-CgoK' | base64 -d 
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y apache2
+
+echo "<html>
+<header><title>This is title</title></header>
+<body>
+Hello world
+</body>
+</html>
+" > /var/www/index.html
+service  apache2 reload 
